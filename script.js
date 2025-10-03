ee24f0b0426dd6c93a5eabbb68fb1facfae54a1d
yamada799 <nakaharaharuna@icloud.com>
2025-10-03 18:48:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleDetail(element) {
   const detail = element.querySelector('.detail');
   if (detail) {
-    detail.style.display = (detail.style.display === 'block') ? 'none' : 'block';
+    detail.classList.toggle('active');
   }
 }
